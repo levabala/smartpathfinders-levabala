f21e0be2947d93d667479c43ed4e1a8fe7e49083
levabala <levabala@ya.ru>
2019-07-15 22:22:52 +0300

    patch update

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -1,11 +1,83 @@
-import { generate } from 'generate-maze-ts';
-import { printMaze } from 'smartpathfinders';
+/* tslint:disable:no-expression-statement no-if-statement no-let*/
+import { Direction } from 'generate-maze-ts';
+import {
+  createFinder,
+  createRoom,
+  exploredMapToMaze,
+  mazeToStrings,
+  p2d,
+  Result,
+  sumDeltas,
+  tryMoveFinder,
+} from 'smartpathfinders';
 
-const maze1 = generate(10, 10);
+const keypress = require('keypress');
 
-const lines = printMaze(maze1);
-const str = lines.join('\n');
+const finder = createFinder({ id: 0 });
+let room = createRoom(10, [{ finder, position: { x: 3, y: 5 } }], 'myseed');
+let result: Result;
 
-console.log(str);
+console.log(mazeToStrings(room.maze, room.positions).join('\n'));
+console.log();
+console.log(finder.exploredMap);
 
-export default 11;
+keypress(process.stdin);
+
+function move(direction: Direction): void {
+  [result, room] = tryMoveFinder(room, room.finders[0], direction);
+
+  console.log(mazeToStrings(room.maze, room.positions).join('\n'));
+  console.log(result);
+
+  const knownBoxes = room.finders[0].exploredMap.reduce(
+    (acc, val) => acc + val.reduce((acc2, val2) => acc2 + (val2 ? 0 : 1), 0),
+    0
+  );
+  console.log({ knownBoxes });
+  console.log(
+    mazeToStrings(
+      exploredMapToMaze(
+        room.finders[0].exploredMap,
+        sumDeltas(room.finders[0].exploredMapOffset, p2d(room.positions[0])),
+        room.maze
+      )
+    ).join('\n')
+  );
+}
+
+process.stdin.on(
+  'keypress',
+  (
+    _,
+    key: {
+      name: string;
+      ctrl: boolean;
+      meta: boolean;
+      shift: boolean;
+      sequence: string;
+      code: string;
+    }
+  ) => {
+    if (key && key.ctrl && key.name === 'c') process.stdin.pause();
+
+    switch (key.name) {
+      case 'up':
+        move(Direction.top);
+        break;
+      case 'right':
+        move(Direction.right);
+        break;
+      case 'down':
+        move(Direction.bottom);
+        break;
+      case 'left':
+        move(Direction.left);
+        break;
+    }
+  }
+);
+
+if (process.stdin.setRawMode) process.stdin.setRawMode(true);
+process.stdin.resume();
+
+export default 0;
